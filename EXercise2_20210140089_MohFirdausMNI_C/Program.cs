899469deba40aca4c39e8deb7414b5f92e55a4b2
mohfirdaus22 <thepashter6@gmail.com>
2022-11-04 16:13:16 +0700

    Add Input

--- EXercise2_20210140089_MohFirdausMNI_C/Program.cs
+++ EXercise2_20210140089_MohFirdausMNI_C/Program.cs
@@ -10,6 +10,15 @@ class Program
 
     public void input()
     {
-
+        while(true)
+        {
+            Console.WriteLine("Enter the number of element in array :");
+            string s = Console.ReadLine();
+            n = Int32.Parse(s);
+            if ((n > 0) && (n <= 109))
+                break;
+            else
+                Console.WriteLine("\n Array should have minimum 1 and maximum 109 element. \n");
+        }
     }
 }
