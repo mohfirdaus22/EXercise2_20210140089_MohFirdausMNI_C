633827f6f1117a01d6797fc7be663bd026fe60c5
mohfirdaus22 <thepashter6@gmail.com>
2022-11-04 16:16:37 +0700

    Add Array element

--- EXercise2_20210140089_MohFirdausMNI_C/Program.cs
+++ EXercise2_20210140089_MohFirdausMNI_C/Program.cs
@@ -9,6 +9,7 @@ class Program
     int i;
 
     public void input()
+        //array input
     {
         while(true)
         {
@@ -19,6 +20,18 @@ class Program
                 break;
             else
                 Console.WriteLine("\n Array should have minimum 1 and maximum 109 element. \n");
+
+        }
+        //accepst array element
+        Console.WriteLine("");
+        Console.WriteLine("-------------------");
+        Console.WriteLine(" Enter the element ");
+        Console.WriteLine("-------------------");
+        for (i = 0; i< n; i++)
+        {
+            Console.Write("<" + (i + 1) + ">");
+            string s1 = Console.ReadLine();
+            maulana[i] = Int32.Parse(s1);
         }
     }
 }
